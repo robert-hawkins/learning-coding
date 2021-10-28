31ae7cc434afac632125e28a78f21e243ec5a44e
robert-hawkins <93270042+robert-hawkins@users.noreply.github.com>
2021-10-28 11:29:19 -0400

    Create lesson-2.rb

---
+++ ruby/lesson-2.rb
@@ -0,0 +1,11 @@
+print "wordth, pleathe! "
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+  user_input.gsub!( /s/, "th" )
+else
+  print "nothing needth changing!"
+end
+
+puts "#{user_input}"
