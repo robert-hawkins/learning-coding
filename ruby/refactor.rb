2aca4c4509209be00f79b6acfb33c467572ec336
robert-hawkins <93270042+robert-hawkins@users.noreply.github.com>
2021-11-01 12:05:00 -0400

    Update refactor.rb

--- ruby/refactor.rb
+++ ruby/refactor.rb
@@ -1,1 +1,12 @@
 #Learn Ruby - Lesson 7b
+#This program was refactored so that it's optimized for Ruby during a lesson
+
+require 'prime'
+
+def first_n_primes( n )
+  return "n must be an integer." unless n.is_a? integer
+  return "n must be greater than 0." if n <= 0
+  Prime.first n
+end
+
+puts first_n_primes( 10 )
