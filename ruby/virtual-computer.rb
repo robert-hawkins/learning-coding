e80b9e2ceb1b0957a304bcf66582fa1e9964791e
robert-hawkins <93270042+robert-hawkins@users.noreply.github.com>
2021-11-02 11:44:34 -0400

    Update virtual-computer.rb

--- ruby/virtual-computer.rb
+++ ruby/virtual-computer.rb
@@ -1,1 +1,21 @@
 #Learn Ruby - Lesson 9b
+
+class Computer
+  @@users = Hash.new
+  def inititalize(username, password)
+    @username = username
+    @password = password
+    @@users[username] = password
+    @files = Hash.new
+  end
+  def create(filename)
+    time = Time.now
+    @files[@filename] = time
+    puts "#{filename} was created by #{username} at #{time}."
+  end
+  def Computer.get_users
+    return @@users
+  end
+end
+
+my_computer = Computer.new("Blake", "password")
